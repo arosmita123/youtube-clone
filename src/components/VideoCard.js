eb37ce4e96690137cb3ae270f31ee46a51ec6e47
Aros <arosmita.jena@gmail.com>
2023-05-20 10:09:06 +0530

    design changed

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -6,8 +6,8 @@ const VideoCard = ({ info }) => {
     // const { snippet, statistics } = info;
     // const { channelTitle, title, thumbnails } = snippet
     return (
-        <div className='p-2 m-2  shadow-lg  min-h-72 w-[350px] my-10  hover:cursor-pointer'>
-            <img className="h-48 w-[350px] object-cover rounded-xl hover:rounded-none duration-300" src={info.snippet.thumbnails.medium.url} alt='thumbnail' />
+        <div className='p-2 m-2  shadow-lg  min-h-72 w-[400px] my-10  hover:cursor-pointer'>
+            <img className="h-48 w-[400px] object-cover rounded-xl hover:rounded-none duration-300" src={info.snippet.thumbnails.medium.url} alt='thumbnail' />
             <div>
                 <div className='font-bold py-2'>{info.snippet.title}</div>
                 <div className='flex flex-row justify-between font-bold text-slate-600'>
